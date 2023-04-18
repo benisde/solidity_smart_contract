736803951670da0295c2387563f2aae3a929eb91
Beni <benishankarde@gmail.com>
2023-04-17 20:06:37 -0400

    updated comments in the solidity

--- joint_savings.sol
+++ joint_savings.sol
@@ -1,10 +1,11 @@
-// SPDX-License-Identifier: MIT
 /*
 Joint Savings Account
 ---------------------
 
-To automate the creation of joint savings accounts, we will create a solidity smart contract that accepts two user addresses that are then able to control a joint savings account. 
-Our smart contract will use ether management functions to implement various requirements from the financial institution to provide the features of the joint savings account.
+To automate the creation of joint savings accounts, we will create a solidity smart contract that accepts two user 
+addresses that are then able to control a joint savings account. 
+Our smart contract will use ether management functions to implement various requirements from the financial institution
+ to provide the features of the joint savings account.
 
 We will do the following in this Smart Contract:
 
@@ -43,17 +44,20 @@ contract JointSavings {
     function withdraw(uint amount, address payable recipient) public {
 
         /*
-        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. The `requiere` statement returns the text `"You don't own this account!"` if it does not.
+        Define a `require` statement that checks if the `recipient` is equal to either `accountOne` or `accountTwo`. 
+        The `requiere` statement returns the text `"You don't own this account!"` if it does not.
         */
         require(recipient == accountOne || recipient == accountTwo, "You don't own this account!" );
 
         /*
-        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation. If there are insufficient funds, the text `Insufficient funds!` is returned.
+        Define a `require` statement that checks if the `balance` is sufficient to accomplish the withdraw operation.
+        If there are insufficient funds, the text `Insufficient funds!` is returned.
         */
         require(address(this).balance >= amount, "Insufficient funds!");
 
         /*
-        Add an `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient`. If it's not equal, set it to the current value of `recipient`.
+        Add an `if` statement to check if the `lastToWithdraw` is not equal to (`!=`) to `recipient`. If it's not 
+        equal, set it to the current value of `recipient`.
         */
         if (lastToWithdraw != recipient){
             lastToWithdraw = recipient;
@@ -62,10 +66,13 @@ contract JointSavings {
         // Set  `lastWithdrawAmount` equal to `amount`
         lastWithdrawAmount = amount;
 
-        // Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` to reflect the new balance of the contract.
+        /* 
+        Call the `contractBalance` variable and set it equal to the balance of the contract by using `address(this).balance` 
+        to reflect the new balance of the contract.
+         */
         contractBalance = address(this).balance - amount;
 
-        // Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument.
+        /* Call the `transfer` function of the `recipient` and pass it the `amount` to transfer as an argument. */
         return msg.sender.transfer(amount);
     }
 
